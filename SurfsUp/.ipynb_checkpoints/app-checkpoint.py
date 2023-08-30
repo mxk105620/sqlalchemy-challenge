3df57abbce3d0b610c75061f4be2ece972234ac5
mxk105620 <monilakhadka@Monilas-Air.attlocal.net>
2023-08-30 14:49:31 -0500

    my final submission hopefully

---
+++ SurfsUp/.ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,117 @@
+# Import the dependencies.
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station= Base.classes.station
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return the JSON representation of precipitation data for the last 12 months."""
+    # Calculate the date one year from the last date in the dataset
+    one_year_ago = datetime.strptime(most_recent_date, '%Y-%m-%d') - timedelta(days=365)
+    
+    # Query the precipitation data for the last 12 months
+    results = session.query(Measurement.date, Measurement.prcp)\
+        .filter(Measurement.date >= one_year_ago)\
+        .all()
+    
+    # Convert the results to a dictionary
+    precipitation_data = {date: prcp for date, prcp in results}
+    
+    return jsonify(precipitation_data)
+
+
+@app.route("/api/v1.0/stations")
+def station_names():
+    """Return a JSON list of stations from the dataset."""
+    # Query the stations
+    results = session.query(Station.station).all()
+    
+    # Convert the results to a list
+    stations_list = list(np.ravel(results))
+    
+    return jsonify(stations_list)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return a JSON list of temperature observations for the most active station for the last 12 months."""
+    # Query the temperature observations for the most active station
+    results = session.query(Measurement.date, Measurement.tobs)\
+        .filter(Measurement.station == most_active_station)\
+        .filter(Measurement.date >= one_year_ago)\
+        .all()
+    
+    # Convert the results to a list of dictionaries
+    tobs_data = [{"date": date, "tobs": tobs} for date, tobs in results]
+    
+    return jsonify(tobs_data)
+
+
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def temperature_range(start, end=None):
+    """Return a JSON list of the minimum, average, and maximum temperature for the specified date range."""
+    # Query the temperature data based on the provided start and end dates
+    if end:
+        results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+            .filter(Measurement.date >= start)\
+            .filter(Measurement.date <= end)\
+            .all()
+    else:
+        results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+            .filter(Measurement.date >= start)\
+            .all()
+
+    # Convert the results to a list of dictionaries
+    temperature_range_data = [{"TMIN": tmin, "TAVG": tavg, "TMAX": tmax} for tmin, tavg, tmax in results]
+
+    return jsonify(temperature_range_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
